27793ad8203d16553587603fff42eb2790d48dce
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2025-03-22 14:52:17 -0300

    initialized project

---
+++ src/main/java/br/com/entusiastadamobilidade/menagerfiles/MenagerfilesApplication.java
@@ -0,0 +1,13 @@
+package br.com.entusiastadamobilidade.menagerfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenagerfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenagerfilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/entusiastadamobilidade/menagerfiles/MenagerfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.entusiastadamobilidade.menagerfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenagerfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
